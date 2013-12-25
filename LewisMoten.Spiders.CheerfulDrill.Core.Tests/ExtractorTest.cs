9a1a0d90a7ea35dc62a4441a1080e97e7b84aa29
Lewis Moten <lewismoten@gmail.com>
2013-12-25 11:18:16 -0500

    Setup first test
    #1

---
+++ LewisMoten.Spiders.CheerfulDrill.Core.Tests/ExtractorTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace LewisMoten.Spiders.CheerfulDrill.Core.Tests
+{
+    [TestFixture]
+    public class ExtractorTest
+    {
+        [Test]
+        public void DefaultValues()
+        {
+            var extractor = new Extractor();
+            Assert.That(extractor.Pattern, Is.Empty);
+        }
+    }
+}
---
+++ LewisMoten.Spiders.CheerfulDrill.Core/Extractor.cs
@@ -0,0 +1,12 @@
+﻿namespace LewisMoten.Spiders.CheerfulDrill.Core
+{
+    public class Extractor
+    {
+        public Extractor()
+        {
+            Pattern = string.Empty;
+        }
+
+        public string Pattern { get; set; }
+    }
+}
