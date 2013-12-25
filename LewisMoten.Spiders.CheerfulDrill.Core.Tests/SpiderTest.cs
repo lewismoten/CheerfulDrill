13a9afada11f54da2722273239d1e581a6416871
Lewis Moten <lewismoten@gmail.com>
2013-12-25 12:42:46 -0500

    Create spider
    Fetches content from files.
    
    #1

---
+++ LewisMoten.Spiders.CheerfulDrill.Core.Tests/SpiderTest.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace LewisMoten.Spiders.CheerfulDrill.Core.Tests
+{
+    [TestFixture]
+    public class SpiderTest
+    {
+        private string _sample1;
+
+        [TestFixtureSetUp]
+        public void BeforeTheFirstTest()
+        {
+            _sample1 = File.ReadAllText("Sample1.html");
+        }
+
+        [Test]
+        public void FileContentsPlacedInBasket()
+        {
+            string path = "Sample1.html";
+            var spider = new Spider();
+            spider.Crawl(path);
+            Assert.That(spider.Basket, Is.EqualTo(_sample1));
+        }
+
+        [Test]
+        public void NothingInBasketIfFileNotFound()
+        {
+            const string path = "Sample-Not-Exists.html";
+            var spider = new Spider();
+            spider.Crawl(path);
+            Assert.That(spider.Basket, Is.Null.Or.Empty);
+        }
+
+        [Test]
+        public void SupportsAbsolutePath()
+        {
+            string path = Path.GetFullPath("Sample1.html");
+            var spider = new Spider();
+            spider.Crawl(path);
+            Assert.That(spider.Basket, Is.EqualTo(_sample1));
+        }
+    }
+}
---
+++ LewisMoten.Spiders.CheerfulDrill.Core/Spider.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace LewisMoten.Spiders.CheerfulDrill.Core
+{
+    public class Spider
+    {
+        public string Basket { get; set; }
+
+        public void Crawl(string path)
+        {
+            if (File.Exists(path))
+            {
+                Basket = File.ReadAllText(path);
+            }
+        }
+    }
+}
